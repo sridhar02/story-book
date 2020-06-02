ce19987022087a4e35021046f449ce7eea007c64
sridhar02 <kattasridhar02@gmail.com>
2020-06-02 16:13:33 +0530

    Styling

--- src/components/stories.js
+++ src/components/stories.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-// import { useHistory } from "react-router-dom";
-
 import Axios from "axios";
+import { useHistory } from "react-router-dom";
 
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -10,29 +9,64 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import { Typography,Divider } from "@material-ui/core";
+import { Typography, Divider, makeStyles } from "@material-ui/core";
 
 import Navbar from "./navbar";
 
+const useStoriesStyles = makeStyles((theme) => ({
+  text: {
+    textAlign: "center",
+    margin: theme.spacing(1),
+  },
+  selected: {
+    backgroundColor: "#ccc !important",
+  },
+  tableRowSelected: {
+    "&:hover": {
+      cursor: "pointer",
+      background: "#aaa",
+    },
+  },
+  tableHeadCell: {
+    fontWeight: "bold",
+    fontSize:"18px"
+  },
+}));
+
 function UserTable({ stories }) {
+  const history = useHistory();
+  const classes = useStoriesStyles();
+
+  const openStory = (story) => {
+    history.push(`/story/${story.id}`);
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell>Summary</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Type</TableCell>
-            <TableCell>Complexity</TableCell>
-            <TableCell>Estimated Hours</TableCell>
-            <TableCell>Cost</TableCell>
+            <TableCell className={classes.tableHeadCell}>Id</TableCell>
+            <TableCell className={classes.tableHeadCell}>Summary</TableCell>
+            <TableCell className={classes.tableHeadCell}>Description</TableCell>
+            <TableCell className={classes.tableHeadCell}>Type</TableCell>
+            <TableCell className={classes.tableHeadCell}>Complexity</TableCell>
+            <TableCell className={classes.tableHeadCell}>
+              Estimated Hours
+            </TableCell>
+            <TableCell className={classes.tableHeadCell}>Cost</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {stories &&
             stories.map((story) => (
-              <TableRow key={story.id}>
+              <TableRow
+                key={story.id}
+                onClick={() => openStory(story)}
+                // selected={showButtons === i}
+                className={classes.tableRowSelected}
+                classes={{ selected: classes.selected }}
+              >
                 <TableCell>{story.id}</TableCell>
                 <TableCell>{story.summary}</TableCell>
                 <TableCell>{story.description}</TableCell>
@@ -49,18 +83,24 @@ function UserTable({ stories }) {
 }
 
 function AdminStories({ stories }) {
+  const classes = useStoriesStyles();
   return (
     <>
-      <Typography variant="h6">Admin Stories</Typography>
+      <Typography className={classes.text} variant="h6">
+        Admin Stories
+      </Typography>
       <UserTable stories={stories} />
     </>
   );
 }
 
 function UserStories({ stories }) {
+  const classes = useStoriesStyles();
   return (
     <div>
-      <Typography variant="h6">User Stories</Typography>
+      <Typography className={classes.text} variant="h6">
+        User Stories
+      </Typography>
       <UserTable stories={stories} />
     </div>
   );
@@ -69,6 +109,7 @@ function UserStories({ stories }) {
 export default function Stories() {
   const [role, setRole] = useState("");
   const [stories, setStories] = useState("");
+
   const fetchStories = async () => {
     const userToken = localStorage.getItem("token");
     try {
@@ -86,12 +127,12 @@ export default function Stories() {
       console.log(error);
     }
   };
+
   useEffect(() => {
     fetchStories();
     setRole(localStorage.getItem("role"));
   }, []);
 
-  console.log(role);
   return (
     <>
       <Navbar />
