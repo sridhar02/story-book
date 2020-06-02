74af61fc5b3d39a64e605085a1783b2d643c3b0f
sridhar02 <kattasridhar02@gmail.com>
2020-06-02 15:13:03 +0530

    App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import Login from "./components/login";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Login />
+    </>
   );
 }
 
