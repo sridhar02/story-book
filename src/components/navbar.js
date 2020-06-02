1fd66d6ee74e1da6829116ff77a8f8e6140d239e
sridhar02 <kattasridhar02@gmail.com>
2020-06-02 15:12:36 +0530

    Navbar Component

---
+++ src/components/navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+import { Button, makeStyles, Typography } from "@material-ui/core";
+
+const useNavbarStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    justifyContent: "space-between",
+    [theme.breakpoints.down("sm")]: {
+      // ustifyContent: "none",
+      display: "block",
+      marginTop: theme.spacing(1),
+    },
+  },
+  spacing: {
+    margin: theme.spacing(2, 0, 3, 0),
+  },
+}));
+
+export default function Navbar() {
+  const classes = useNavbarStyles();
+  const history = useHistory();
+
+  const newStory = () => {
+    history.push("/new");
+  };
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Story Book</Typography>
+      <div className={classes.spacing}>
+        <Button variant="contained" onClick={newStory}>
+          +Create new Story
+        </Button>
+        <Button>Logout</Button>
+      </div>
+    </div>
+  );
+}
