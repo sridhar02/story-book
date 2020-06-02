d18cb8366b9e4eb172f3d01a66087ad7eb2df1ee
sridhar02 <kattasridhar02@gmail.com>
2020-06-02 15:13:29 +0530

    routes component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import App from "./App";
+import Stories from "./components/stories";
+import CreateStory from "./components/createStory";
+
+function Main() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={App} exact />
+        <Route path="/stories" component={Stories} exact />
+        <Route path="/new" component={CreateStory} exact />
+      </Switch>
+    </Router>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
