492bb472499cc7c4e95dd20a7a2be60b2f722a3a
sridhar02 <kattasridhar02@gmail.com>
2020-06-03 08:46:00 +0530

    remove  App component

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import App from "./App";
 import Stories from "./components/stories";
 import Story from "./components/story";
 import CreateStory from "./components/createStory";
+import Login from "./components/login";
+
 
 function Main() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={App} exact />
+        <Route path="/" component={Login} exact />
         <Route path="/stories" component={Stories} exact />
         <Route path="/story/new" component={CreateStory} exact />
         <Route path="/story/:id" component={Story} exact />
